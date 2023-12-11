e6008aeb5b03a7f8ce93a1a118f2fa7a4368467d
vrvno <bruno.torres.ar@gmail.com>
2023-12-10 22:16:15 -0300

    activity carnetMedico

---
+++ app/src/main/java/com/example/carnetdigital/Vista/carnetMedico.java
@@ -0,0 +1,16 @@
+package com.example.carnetdigital.Vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.carnetdigital.R;
+
+public class carnetMedico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carnet_medico);
+    }
+}
