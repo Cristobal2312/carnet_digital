a5e6f35915cd3a490783f39b0f7e3288ed7e4490
IDG9I <damiangamerxd123@gmail.com>
2023-12-12 20:22:44 -0300

    Carnet DIGITAL FIX 7.5v NUEVA VISTAS

---
+++ app/src/main/java/com/example/carnetdigital/MSGNuevaCitaMedica.java
@@ -0,0 +1,14 @@
+package com.example.carnetdigital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MSGNuevaCitaMedica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msgnueva_cita_medica);
+    }
+}
