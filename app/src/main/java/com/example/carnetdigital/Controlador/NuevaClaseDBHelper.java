a5e4b4a737b5c893a3688bc2ad2dff5b50da8c02
crist <cristobalnahuel2002@gmail.com>
2023-12-13 02:05:22 -0300

    8.7

---
+++ app/src/main/java/com/example/carnetdigital/Controlador/NuevaClaseDBHelper.java
@@ -0,0 +1,36 @@
+package com.example.carnetdigital.Controlador;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class NuevaClaseDBHelper extends SQLiteOpenHelper {
+
+    // Cambia el nombre de la base de datos aquí
+    private static final String DATABASE_NAME = "NuevoNombreDeLaBD";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String CREATE_TABLE_AGENDA = "CREATE TABLE agenda " +
+            "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "nombrePaciente TEXT, " +
+            "numeroFicha TEXT, " +
+            "fecha TEXT, " +
+            "hora TEXT, " +
+            "boxSala TEXT, " +
+            "terapeuta TEXT, " +
+            "rutPaciente TEXT);";
+
+    public NuevaClaseDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_AGENDA);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Manejar actualizaciones de la base de datos si es necesario
+    }
+}
--- app/src/main/java/com/example/carnetdigital/Vista/RegistrarAgendaHora.java
+++ app/src/main/java/com/example/carnetdigital/Vista/RegistrarAgendaHora.java
@@ -1,35 +1,69 @@
 package com.example.carnetdigital.Vista;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.carnetdigital.Controlador.NuevaClaseDBHelper;
 import com.example.carnetdigital.R;
 
 public class RegistrarAgendaHora extends AppCompatActivity {
+
+    private NuevaClaseDBHelper dbHelper; // Cambiado el nombre de DBHelper a NuevaClaseDBHelper
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_agenda_hora);
 
-        EditText txtMedico = findViewById(R.id.txtTerapeuta);
-        EditText txtBox = findViewById(R.id.txtBox);
-        EditText txtFecha = findViewById(R.id.txtFecha);
-        EditText txtHorario = findViewById(R.id.txtHorario);
-        Button btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
+        dbHelper = new NuevaClaseDBHelper(this); // Cambiado el nombre de DBHelper a NuevaClaseDBHelper
 
+        Button btnRegistrar = findViewById(R.id.btnRegistrar);
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                Intent sIntent = new Intent(RegistrarAgendaHora.this, MostrarAgendaHora.class);
-                startActivity(sIntent);
+            public void onClick(View v) {
+                // Al hacer clic en el botón Registrar, recoger los datos de las vistas
+                insertarDatosEnDB();
             }
         });
+    }
+
+    private void insertarDatosEnDB() {
+        EditText txtNombrePaciente = findViewById(R.id.txtNombrePaciente);
+        EditText txtNumFicha = findViewById(R.id.txtNumFicha);
+        EditText txtFecha = findViewById(R.id.txtFecha);
+        EditText txtHorario = findViewById(R.id.txtHorario);
+        EditText txtBox = findViewById(R.id.txtBox);
+        EditText txtTerapeuta = findViewById(R.id.txtTerapeuta);
+        EditText txtRutPaciente = findViewById(R.id.txtRutPaciente);
+
+        // Obtener los valores de las vistas
+        String nombrePaciente = txtNombrePaciente.getText().toString();
+        String numeroFicha = txtNumFicha.getText().toString();
+        String fecha = txtFecha.getText().toString();
+        String horario = txtHorario.getText().toString();
+        String box = txtBox.getText().toString();
+        String terapeuta = txtTerapeuta.getText().toString();
+        String rutPaciente = txtRutPaciente.getText().toString();
+
+        // Insertar en la base de datos
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("nombrePaciente", nombrePaciente);
+        values.put("numeroFicha", numeroFicha);
+        values.put("fecha", fecha);
+        values.put("hora", horario);
+        values.put("boxSala", box);
+        values.put("terapeuta", terapeuta);
+        values.put("rutPaciente", rutPaciente);
+
+        long newRowId = db.insert("agenda", null, values);
+
+        // Cierra la conexión de la base de datos
+        dbHelper.close();
     }
 }
