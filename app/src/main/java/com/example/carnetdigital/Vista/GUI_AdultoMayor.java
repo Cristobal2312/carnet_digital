ae0a081cf061ab8b5e72ddcbab2830db54138592
IDG9I <damiangamerxd123@gmail.com>
2023-12-12 22:07:19 -0300

    Carnet DIGITAL FIX 8.0v Vista PreTERMINADA

--- app/src/main/java/com/example/carnetdigital/Vista/GUI_AdultoMayor.java
+++ app/src/main/java/com/example/carnetdigital/Vista/GUI_AdultoMayor.java
@@ -2,7 +2,9 @@ package com.example.carnetdigital.Vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.carnetdigital.R;
@@ -17,6 +19,21 @@ public class GUI_AdultoMayor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gui_adulto_mayor);
+
+        bntMas = (Button) findViewById(R.id.CbtnMas);
+
+
+
+
+        bntMas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GUI_AdultoMayor.this, GUI_AdultoMayorPAG_2.class);
+                startActivity(intent);
+            }
+        });
+
+
 
 
     }
